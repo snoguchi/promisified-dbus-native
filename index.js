264bdad1450c2cfc9bf093367875f416afceaadb
Shinsuke Noguchi <shinsuke.noguchi@gmail.com>
2018-06-13 21:04:56 +0900

    bugfix

--- index.js
+++ index.js
@@ -30,37 +30,35 @@ function promisifyInterface(iface) {
 }
 
 function promisifyObject(obj) {
-  const promisified = {};
+  obj._promisifiedInterface = {};
   obj.as = function(name) {
-    promisified[name] = promisified[name] || promisifyInterface(this.proxy[name]);
-    return promisified[name];
+    if (!(name in this._promisifiedInterface)) {
+      return this._promisifiedInterface[name] = promisifyInterface(this.proxy[name]);
+    }
+    return this._promisifiedInterface[name];
   }
   return obj;
 }
 
-
 const createClient = dbus.createClient;
 dbus.createClient = function() {
   const bus = createClient.apply(dbus, arguments);
-
   const getService = bus.getService;
   bus.getService = function(name) {
     const service = getService.call(bus, name);
     const getObject = service.getObject;
     service.getObject = function(name, callback) {
-      if (typeof callback === 'undefined') {
-        return new Promise((resolve, reject) => {
-          getObject.call(service, name, (err, obj) => {
-            err ? reject(err) : resolve(promisifyObject(obj));
-          });
-        });
-      } else {
+      if (typeof callback !== 'undefined') {
         return getObject.call(service, name, callback);
       }
+      return new Promise((resolve, reject) => {
+        getObject.call(service, name, (err, obj) => {
+          err ? reject(err) : resolve(promisifyObject(obj));
+        });
+      });
     }
     return service;
   }
-
   return bus;
 }
 
